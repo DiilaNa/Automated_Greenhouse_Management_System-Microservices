389f0819c7897a200932a1b06763deae764738ec
DiilaNa <liyanaarachchidilan@gmail.com>
2026-03-06 19:15:21 +0530

    feat:regiter model mapper and bcrypt as beans in application config

---
+++ Auth-Service/src/main/java/lk/dilan/project/authservice/config/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package lk.dilan.project.authservice.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+}
