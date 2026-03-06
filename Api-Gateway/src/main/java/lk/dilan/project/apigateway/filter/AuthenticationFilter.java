799e3f0484b86e7b24e698305831b662c17d6a5b
DiilaNa <liyanaarachchidilan@gmail.com>
2026-03-06 15:45:04 +0530

    feat: add authentication filter class

---
+++ Api-Gateway/src/main/java/lk/dilan/project/apigateway/filter/AuthenticationFilter.java
@@ -0,0 +1,38 @@
+package lk.dilan.project.apigateway.filter;
+
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    public static class Config {
+
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Missing Authorization Header");
+            }
+
+            String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                return chain.filter(exchange);
+            } else {
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid Token");
+            }
+        };
+    }
+}
