bf5b382bc731283ec55fdeae4c4685fa5b95d34b
DiilaNa <liyanaarachchidilan@gmail.com>
2026-03-27 09:04:27 +0530

    feat:mongoDB connected inside indes ts

--- zone-service/src/index.ts
+++ zone-service/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello, World!");
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const SERVER_PORT = process.env.PORT || 8081;
+const MONGO_URL = process.env.MONGO_URI as string;
+
+const app = express();
+app.use(express.json());
+
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("✅ Zone Service connected to MongoDB"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
+
+app.listen(SERVER_PORT, () => {
+  console.log(`🚀 Zone Service running on port ${SERVER_PORT}`);
+});
